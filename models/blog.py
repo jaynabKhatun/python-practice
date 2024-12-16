605b4e15e372ca762da184c8064d107faadf226f
unknown <anisamim94@gmail.com>
2024-12-17 03:58:56 +0600

    added crud oparation in fast api iwth py

--- models/blog.py
+++ models/blog.py
@@ -7,3 +7,11 @@ class BlogModel(BaseModel):
     content: str
     author: str
     tags: list
+
+
+class UpdateBlogModel(BaseModel):
+    title: str = None
+    subtitle: str = None
+    content: str = None
+    author: str = None
+    tags: list = None
--- routes/blog.py
+++ routes/blog.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from models.blog import BlogModel
+from models.blog import BlogModel, UpdateBlogModel
 from config.config import blog_collection
 from serializes.blog import DecodeBlogs, DecodeBlog
 from bson import ObjectId
@@ -41,7 +41,7 @@ def allBlogs():
     }
 
 
-# single blog
+# get single blog
 
 @blog_root.get("/blog/{_id}")
 def getBlog(_id):
@@ -51,4 +51,35 @@ def getBlog(_id):
     return {
         "status": "ok",
         "blog": Decode_data
+    }
+
+
+# update a single blog
+
+@blog_root.patch("/update/{_id}")
+def updateBlog(_id: str, doc: UpdateBlogModel):
+    req = dict(doc.model_dump(exclude_unset=True))
+    blog_collection.find_one_and_update(
+        {"_id": ObjectId(_id)},
+        {"$set": req}
+    )
+
+    return {
+        "status": "ok",
+        "message": "Blog updated successfully"
+    }
+
+
+# deleted a blog
+
+
+@blog_root.delete("/delete/{_id}")
+def deletedBlog(_id: str):
+    blog_collection.find_one_and_delete(
+        {"_id": ObjectId(_id)}
+
+    )
+    return {
+        "status": "ok",
+        "message": "Blog deleted successfully"
     }
